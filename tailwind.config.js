08e8b894b17f120ad1a5ebb756fb2cc66c552443
HyakujuNoKaido <128378173+HyakujuNoKaido@users.noreply.github.com>
2026-09-14 14:00:20 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'class', // Mode sombre prioritaire
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
